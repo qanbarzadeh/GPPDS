229fbc96a22f0a6914baf141c5e0c9518ba43cc3
dante825 <kangwei9125@gmail.com>
2018-04-28 15:59:56 +0800

    Added the buy or sell indicator but in a separate chart since has problem to merge the charts

--- app.R
+++ app.R
@@ -1,13 +1,14 @@
 library(shiny)
 library(quantmod)
 library(TTR)
+library(binhf)
 
 # A predefined list of stocks
 stocks <- c('Apple' = 'AAPL', 'Google' = 'GOOG', 'Wallmart' = 'WMT','Amazon'='AMZN',
             'S&P 500' = '^GSPC','NASDAQ'='^IXIC')
 features <- c('Bollinger Bands'='addBBands()', 'Volume'='addVo()',
-             'Commodity Channel Index'='addCCI()', 'Simple Moving Average'='addSMA()',
-             'Rate of Change' = 'addROC()', 'Exponential Moving Average'='addEMA()')
+             'Commodity Channel Index'='addCCI()', 'Simple Moving Average (red)'='addSMA()',
+             'Exponential Moving Average (blue)'='addEMA()', 'Rate of Change' = 'addROC()')
 averages <- c('Fast moving average'=1, 'Slow moving average'=2)
 
 # The UI part
@@ -22,6 +23,9 @@ ui <- fluidPage(
     # Slider for user to select time frame
     sliderInput(inputId='time', label='Time Period',
                 min = 1, max = 365, value = 30),
+    # Radio button for theme selection
+    radioButtons(inputId = 'theme', label='Theme', choices=c('White'=1, 'Black'=2),
+                 selected=1),
     # Checkbox for user to select features to show in chart
     checkboxGroupInput(inputId='features', label='Features', choices=features,
                        selected='addBBands()'),
@@ -32,53 +36,73 @@ ui <- fluidPage(
   mainPanel(
     plotOutput('price_plot'),
     br(),
-    dataTableOutput('stock_table')
-    #textOutput('selected_stock'),
-    #textOutput('selected_time'),
-    #textOutput('selected_features'),
-    #tabsetPanel(
-    #  id = 'table',
-    #  tabPanel('stock', DT::dataTableOutput('stockTable'))
-    #)
+    plotOutput('predict_plot'),
+    br()
+    #dataTableOutput('stock_table')
   )
 )
 
 # The main function that handle input from UI and output graphs
 server <- function(input, output, session) {
-  # Checking the input is obtained correctly
-  #output$selected_stock <- renderText({
-  #  paste("You have selected ", input$stock)
-  #  })
-  #output$selected_time <- renderText({
-  #  paste("You have selected this time period", input$time)
-  #})
-  #output$selected_features <- renderText({
-  #  paste(input$features, collapse=';')
-  #})
-  #output$stockTable <- DT::renderDataTable({
-  #  stock <- getSymbols(input$stock, src='yahoo', auto.assign = FALSE)
-  #  DT::datatable(stock, options = list(orderClasses=TRUE))
-  #})
   output$stock_table <- renderDataTable({
     stock <- getSymbols(input$stock, src='yahoo', auto.assign = FALSE)
-    #sub_stock <- stock[, 4, drop=FALSE]
   })
+  
   # Output the chart
   output$price_plot <- renderPlot({
     stock <- getSymbols(input$stock, src='yahoo', auto.assign = FALSE)
-    ema.10 <- EMA(stock[, 4, drop=FALSE], n=10, ) 
-    ema.50 <- EMA(stock[, 4, drop=FALSE], n=50, ) 
-    ema.200 <- EMA(stock[, 4, drop=FALSE], n=200, )
+    # Theme color selection
+    themeCol <- 'white'
+    if (input$theme==1) {
+      themeCol <- 'white'
+    } else {
+      themeCol <- 'black'
+    }
+    
+    # Calculating the EMA for the moving averages
+    ema.10 <- EMA(stock[, 4, drop=FALSE], n=10) 
+    ema.50 <- EMA(stock[, 4, drop=FALSE], n=50) 
+    ema.200 <- EMA(stock[, 4, drop=FALSE], n=200)
     fastDifference <- ema.10 - ema.50
     slowDifference <- ema.50 - ema.200
-    chartSeries(stock,theme = "white",TA=paste(input$features, collapse = ';'),
-                subset = paste('last', input$time, "days"))
     
+    # Plotting the main chart
+    chartSeries(stock,name='Stock Price Movement', theme=themeCol, TA=paste(input$features, collapse = ';'),
+                subset = paste('last', input$time, "days"), show.grid=TRUE)
+    # Plot the average based on user input
     if (input$average==1) {
       addTA(fastDifference, on=NA, col='blue', legend="Fast moving average")  
     } else {
-      addTA(slowDifference, on=NA, col='red', legend="Slow moving average") 
+      addTA(slowDifference, on=NA, col='blue', legend="Slow moving average") 
     }
+  })
+  
+  output$predict_plot <- renderPlot({
+    stock <- getSymbols(input$stock, src='yahoo', auto.assign = FALSE)
+    
+    # Calculating the EMA for the moving averages
+    ema.10 <- EMA(stock[, 4, drop=FALSE], n=10) 
+    ema.50 <- EMA(stock[, 4, drop=FALSE], n=50) 
+    ema.200 <- EMA(stock[, 4, drop=FALSE], n=200)
+    fastDifference <- ema.10 - ema.50
+    slowDifference <- ema.50 - ema.200
+    
+    buySignal <- ifelse(
+      slowDifference  > 0 &
+        fastDifference  > 0 &
+        shift(v=as.numeric(fastDifference), places=1, dir="right") < 0, stock[, 4, drop=FALSE], NA)
+    
+    # look for long exits (same thing but inverse signts)
+    sellSignal <- ifelse(
+      slowDifference  < 0 &
+        fastDifference  < 0 &
+        shift(v=as.numeric(fastDifference), places=1, dir="right") > 0, stock[, 4, drop=FALSE], NA)
+    
+    plot(stock[, 4, drop=FALSE],type='l',grid.col='white',grid2='white',
+         subset='2015::2018', main='Buying and Selling oppotunity')
+    
+    points(buySignal, col='blue', cex=3, pch=17)
+    points(sellSignal, col='red',bg='red', cex=3, pch=25)
   })
   
 }
